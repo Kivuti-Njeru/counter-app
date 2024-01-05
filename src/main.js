bd12bb9d0565568b995472bd72aa6b921dd89618
Alexander <77789033+Kivuti-Njeru@users.noreply.github.com>
2024-01-05 13:24:26 +0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
+let count = 0
 
+let value = document.querySelector('.main__count')
+const btns = document.querySelectorAll('.btn')
+ value.style.color = '#888'
+ value.textContent = count
+
+btns.forEach(
+    function(btn){
+        btn.addEventListener('click', (e) => {
+            const styles = e.currentTarget.classList
+            if(styles.contains('decrease')){
+                count--
+            }else if (styles.contains('increase')){
+                count++
+            }else{
+                count = 0
+            }
+            if(count > 0 ){
+                value.style.color = '#32cd32'
+            }
+            if(count < 0 ){
+                value.style.color = '#ff6347'
+            }
+            if(count == 0 ){
+                value.style.color = '#888'
+            }
+            value.textContent = count
+        })
+    }
+)
